f8e0eb475ae87e0d55dc4bf2fa8bd23f1ec1eb78
loch <nigelloch@gmail.com>
2022-01-22 11:10:21 -0600

    completed constructor classes

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,2 +1,12 @@
 const Employee = require('..lib/Employee.js');
+const { expect } = require('@jest/globals');
 
+
+
+test('creates an employee object', () => {
+    const employee = new Employee('Nigel', 5, 'nigelloch@gmail.com ');
+
+    expect(employee.name).toBe(String);
+    expect(employee.id).toBe(Number);
+    expect(employee.email).toBe(String);
+})
